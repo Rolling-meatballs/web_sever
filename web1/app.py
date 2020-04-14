babfd4ff35f483415fe8c98bfe8d33f6caa9682e
Rolling_meatballs <yangfan__8123@163.com>
2020-04-14 10:17:51 +0800

    rebuild routs

--- web1/app.py
+++ web1/app.py
@@ -32,9 +32,9 @@ def current_time():
 def configured_app():
     url_map = {}
     app = Flask(__name__)
-    # app.register_blueprint(public_bp)
-    # app.register_blueprint(user_bp)
-    # app.register_blueprint(weibo_bp)
+    app.register_blueprint(public_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(weibo_bp)
     SQLModel.init_db()
 
     app.errorhandler(404)(error_view)
